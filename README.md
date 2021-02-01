636eab1ffbe76424ee1e0353521a9c437096e03c
unknown <sndchrs440@gmail.com>
2021-02-01 18:44:22 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # userMgmt
 User Registration and Login System
+Project Install
+
+Step 1: Run composer update command
+composer update
+
+Step 2: Connect Database
+
+Step 3: Run migration command
+php artisan migrate
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('mobile_no');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('profile_picture')->nullable();
+            $table->integer('pincode');
+            $table->string('aadhar_card_no');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
